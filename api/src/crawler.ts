a8a3757488390698b5bfb71b0135d618cd51ec28
Admin <admin@LTLIBRARY.ORG>
2024-04-29 17:17:47 -0500

    implements get_date_string functionfor formatting a date string to be used in the web crawler operations

--- api/src/crawler.ts
+++ api/src/crawler.ts
@@ -10,11 +10,26 @@ type calendar_event = {
     time: string
 }
 
-/* CLIENT will provide the upcomingweek to get a calendar date from
-___________________________________________________________________*/
-const week_date: Date = new Date();
-console.log(week_date);
+/* CLIENT will provide the upcoming week from which
+    to get a calendar date
+    - calendar classes are listed as:
+    - "tribe-events-calendar-day-YYYY-MM-DD"
+    - prefix: "tribes-events-calendar-day"
+    - suffix: "YYYY-MM-DD"
+*/
+const date_prefix: string = "tribe-events-calendar-day-";
+const date_string_length: number = 10;
 
+function get_date_string(
+    date_prefix: string,
+    date_object: Date, 
+    string_length: number): 
+    string {
+        return (
+            date_prefix + 
+            date_object.toISOString().slice(0, string_length)
+        )
+}
 /* REQUIREMENTS
     - navigate to meeting room calendar page
     - find the upcoming week via class name or text content
