0d8f5309a89ebca0d53ff7661140eb2d829aac4c
Admin <admin@LTLIBRARY.ORG>
2024-04-30 12:43:37 -0500

    added more instructions for self as I explore puppeteer and element selector possibilities

--- api/src/crawler.ts
+++ api/src/crawler.ts
@@ -19,6 +19,8 @@ const date: date_info = {
     date: new Date() // will be received from client
 }
 
+
+
 function get_date_string(date: date_info): string  {
     return (
         date.prefix + date.date.toISOString().slice(0, date.length)
@@ -34,6 +36,22 @@ function get_date_string(date: date_info): string  {
     - on each day, create an object of type calendar_event
         to hold the date, title, and time
     - return the array to the server for response to client
+
+
+    - DATA TO EXTRACT FROM EACH CALENDAR DATE
+    - container: .tribe-events-calendar-month__calendar-event-details
+    - title: .tribe-events-calendar-month__calendar-event-title-link
+    - times: <time datetime="hh:mm">HH:MM AM/PM</time>
+    - date: .tribe-events-calendar-month__day-date-link
+
+    - MOST EFFICIENT PATH TO DATA EXTRACTION
+    - get the beginning weekday from the user-provided date object
+    - evaluate the parent node:
+        .tribe-events-calendar-month__week
+    - parent node may require .closest() for selector
+        - in common js:
+        childNode.closest('.tribe-events-calendar-month__week')
+    - iterate over the above containers to gather data
 */
 
 const calendar: {url: string, date_id: string}  = {
