4e8af542db08122426bca79073ad0f99ede72806
Admin <admin@LTLIBRARY.ORG>
2024-05-07 17:55:34 -0500

    added more tests for string functions

--- api/src/crawler.test.ts
+++ api/src/crawler.test.ts
@@ -1,6 +1,9 @@
 import { testExports as t } from "./crawler";
 
 
+/* formatDateYYYYMMDD()
+____________________________________________________________*/
+
 test('Date objects should return a string MMMM-YY-DD', ()=> {
     expect(
         t.formatDateYYYYMMDD(new Date('Wed May 08 2024 12:00:00')))
@@ -18,3 +21,42 @@ test('Date objects should return a string MMMM-YY-DD', ()=> {
         t.formatDateYYYYMMDD(new Date('Fri Jan 07 2022 08:54:00')))
         .toBe("2022-01-07");
 });
+
+test('Date objects should return a string MMMM-YY-DD', ()=> {
+    expect(
+        t.formatDateYYYYMMDD(new Date('1992, 01, 07')))
+        .toBe("1992-01-07");
+
+    expect(
+        t.formatDateYYYYMMDD(new Date('1998, 05, 01')))
+        .toBe("1998-05-01");
+})
+
+
+/* createTargetIdName()
+____________________________________________________________*/
+
+test('Strings should be returned in format PREFIX + INPUT STRING', ()=> {
+    expect(
+        t.createTargetIdName("1992-01-07"))
+        .toBe("#tribe-events-calendar-day-1992-01-07");
+
+    expect(
+        t.createTargetIdName("1998-05-01"))
+        .toBe("#tribe-events-calendar-day-1998-05-01");
+
+    expect(
+        t.createTargetIdName("2024-05-20"))
+        .toBe("#tribe-events-calendar-day-2024-05-20");
+})
+
+test ('Strings should be the appropriate length', () => {
+    expect(() => {
+        t.createTargetIdName("")
+    }).toThrow("Empty string passed where string expected")
+        
+    expect(() => {
+        t.createTargetIdName("2024-5-2")
+    }).toThrow("String length must equal that of the format YYYY-MM-DD (10)")
+
+})
--- api/src/crawler.ts
+++ api/src/crawler.ts
@@ -1,16 +1,28 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
+
 const targetURL = "https://laketravislibrary.org/meeting-room/";
 const today = new Date();
 const todayString = formatDateYYYYMMDD(today);
-console.log(today);
 
 
 function formatDateYYYYMMDD(date: Date): string {
     // Inputs: a raw date object
     // Outputs: A string of the date in format YYYY-MM-DD
     return date.toISOString().split('T')[0];
+}
+
+function createTargetIdName(dateString: string){
+    if(!dateString){
+        throw new Error("Empty string passed where string expected")
+    }
+    if(dateString.length < 10){
+        throw new Error("String length must equal that of the format YYYY-MM-DD (10)");
+    }
+
+    const classPrefix:string = "#tribe-events-calendar-day-";
+    return classPrefix + dateString;
 }
 
 
@@ -22,9 +34,8 @@ async function main (){
 }
 
 
-/*
-DEXPORT FUNCTIONS FOR 
-*/
+// EXPORT FUNCTIONS FOR TESTING
 export const testExports = {
-    formatDateYYYYMMDD
+    formatDateYYYYMMDD,
+    createTargetIdName
 }
