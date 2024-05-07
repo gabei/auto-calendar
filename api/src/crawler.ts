8f329e6cc043f39a7e49cb316b06b75a4af93cdb
Admin <admin@LTLIBRARY.ORG>
2024-05-07 16:31:36 -0500

    implements formatDateYYYYMMDD funcion.

--- api/src/crawler.ts
+++ api/src/crawler.ts
@@ -2,12 +2,22 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 const targetURL = "https://laketravislibrary.org/meeting-room/";
+const today = formatDateYYYYMMDD(new Date());
+console.log(today);
 
+
+function formatDateYYYYMMDD(date: Date): string {
+    // Inputs: a raw date object
+    // Outputs: A string of the date in format YYYY-MM-DD
+    return date.toISOString().split('T')[0];
+}
 
 
 async function main (){
-    //axios.get(url, [...options]);
     const response = await axios.get(targetURL);
-    const $ = await cheerio.load(response.data);
+    const $ = cheerio.load(response.data);
+
+    const weekSelector = ".tribe-events-calendar-month__week";
+
 
 }
