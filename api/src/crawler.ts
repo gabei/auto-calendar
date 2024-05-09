7b8c791f828502a7cb72cfbc3b94750b8c11e7fd
Admin <admin@LTLIBRARY.ORG>
2024-05-09 18:18:22 -0500

    reordered function calls and type definitions

--- api/src/crawler.ts
+++ api/src/crawler.ts
@@ -1,43 +1,22 @@
 import axios, { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
 
-
+// extract this into env variable later
 const targetURL = "https://laketravislibrary.org/meeting-room/";
 
-
-
-function formatDateYYYYMMDD(date: Date): string {
-    // Inputs: a raw date object
-    // Outputs: A string of the date in format YYYY-MM-DD
-    return date.toISOString().split('T')[0];
+type CalendarDate = {
+    date: number,
+    events: Event[]
 }
 
-function createTargetIdName(dateString: string){
-    if(!dateString){
-        throw new Error("Empty string passed where string expected")
-    }
-    if(dateString.length < 10){
-        throw new Error("String length must equal that of the format YYYY-MM-DD (10)");
-    }
-
-    const classPrefix:string = "#tribe-events-calendar-day-";
-    return classPrefix + dateString;
+type Event = {
+    title: string,
+    time: string
 }
 
 async function main (){
     const response = await axios.get(targetURL);
     const $ = cheerio.load(response.data);
-
-
-    type CalendarDate = {
-        date: number,
-        events: Event[]
-    }
-
-    type Event = {
-        title: string,
-        time: string
-    }
 
     const startingDate = new Date();
     let i: number = 0;
@@ -65,12 +44,26 @@ async function main (){
         i++;
     } while(i < weekLength);
 }
-    
 
 main();
 
+function formatDateYYYYMMDD(date: Date): string {
+    // Inputs: a raw date object
+    // Outputs: A string of the date in format YYYY-MM-DD
+    return date.toISOString().split('T')[0];
+}
 
+function createTargetIdName(dateString: string){
+    if(!dateString){
+        throw new Error("Empty string passed where string expected")
+    }
+    if(dateString.length < 10){
+        throw new Error("String length must equal that of the format YYYY-MM-DD (10)");
+    }
 
+    const classPrefix:string = "#tribe-events-calendar-day-";
+    return classPrefix + dateString;
+}
 
 // EXPORT FUNCTIONS FOR TESTING
 export const testExports = {
