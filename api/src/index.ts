aff50d38145ce5da22ffac53c0fcb7254284f5aa
gabei <gabe1792@gmail.com>
2024-05-11 13:02:09 -0500

    added a new route for calendar data

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send(`The typescript server is running on port ${port}.`);
 })
 
+app.get("/calendar-data", (req: Request, res: Response) => {
+    res.send("This page responds with the scraped meeting room calendar data.");
+})
+
 app.listen(port, () => {
     console.log(`Application listening at port ${port}`);
 });
