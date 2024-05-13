a5e5e9ab02bef54eb82b5e0e756f1907d289fce4
gabei <gabe1792@gmail.com>
2024-05-13 18:47:53 -0500

    added http request. currently does nothing and also violates CORS policy

--- client/src/Calendar/Calendar.tsx
+++ client/src/Calendar/Calendar.tsx
@@ -2,11 +2,35 @@ import { useState } from 'react';
 import './Calendar.css';
 
 export default function Calendar() {
+    const [data, setData] = useState();
+
+
+    function handleClick() {
+        console.log("Making an http request to te local host...");
+        const request = new XMLHttpRequest; 
+
+        request.open('GET', 'http://localhost:3000');
+        request.onload = function() {
+            if(request.status === 200){
+                setData(JSON.parse(request.responseText));
+            }
+        }
+
+        request.send();
+    }
 
     return (
         <div className='Calendar'>
             <label htmlFor="Calendar__input"></label>
-            <input type="date" className="Calendar__input" name="Calendar__input" />
+            <input 
+                type="date" 
+                className="Calendar__input" 
+                name="Calendar__input" />
+            <button 
+                onClick={handleClick}>Get Dates</button>
+            <p>
+                {data || "No data received yet..."}
+            </p>
         </div>
     )
 }
