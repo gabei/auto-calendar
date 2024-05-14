3cb8aaf91cb64c2f2d66efafe20ef723d4c2845b
gabei <gabe1792@gmail.com>
2024-05-14 15:27:19 -0500

    allowed CORS on the express server for requests sent from react. Added API server to proxy settings for react.

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,15 +7,23 @@ dotenv.config();
 const app: Express = express();
 const port: number = 3000;
 
+const cors = require('cors');
+const withOptions = {
+    origin: "http://localhost:5173/" ,
+    succeessStatus: 200
+}
+
+app.use(cors(withOptions));
+
 app.get("/", (req: Request, res: Response) => {
     res.send(`The typescript server is running on port ${port}.`);
 })
 
-app.get("/calendar", async (req: Request, res: Response) => {
+app.get("/calendar", cors(withOptions), async (req: Request, res: Response) => {
     const response: CalendarDate[] = await populateCalendarWeek();
     res.send(response);
 })
 
 app.listen(port, () => {
-    console.log(`Application listening at port ${port}`);
+    console.log(`Application listening at port http://localhost:${port}`);
 });
--- client/src/Calendar/Calendar.tsx
+++ client/src/Calendar/Calendar.tsx
@@ -4,12 +4,11 @@ import './Calendar.css';
 export default function Calendar() {
     const [data, setData] = useState();
 
-
     function handleClick() {
         console.log("Making an http request to te local host...");
         const request = new XMLHttpRequest; 
 
-        request.open('GET', 'http://localhost:3000');
+        request.open('GET', '/calendar');
         request.onload = function() {
             if(request.status === 200){
                 setData(JSON.parse(request.responseText));
