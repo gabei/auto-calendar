000debd46b3a2d560a8ff94ea65197347bc8da81
gabei <gabe1792@gmail.com>
2024-05-20 16:06:45 -0500

    installs react-print and adds a prnit button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.scss'
 import Calendar from './Calendar/Calendar'
-function App() {
 
+function App() {
   const [data , setData] = useState([]);
 
     async function handleClick() {
-        console.log("Making an http request to the proxy server...");
-
         const response = await fetch("http://localhost:3000/calendar", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
@@ -16,8 +14,12 @@ function App() {
         
         const data = await response.json();
         const dates = data.data;
-        console.log(dates);
+    
         setData(dates);
+    }
+
+    function handlePrint() {
+      
     }
 
   return (
@@ -27,8 +29,8 @@ function App() {
                 type="date" 
                 className="Calendar__input" 
                 name="Calendar__input" />
-            <button 
-                onClick={handleClick}>Get Dates</button>
+            <button onClick={handleClick}>Get Dates</button>
+            <button onClick={handlePrint}>Print</button>
 
       {data.length && <Calendar data={data}></Calendar>}
     </div>
