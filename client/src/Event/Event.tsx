697a8cd6ad6e806f0e6d655a7d2ede9a1a919ed2
gabei <gabe1792@gmail.com>
2024-05-29 18:14:18 -0500

    remove over-engineered resizing code to replace with css resize property. adjusted some sizes

--- client/src/Event/Event.tsx
+++ client/src/Event/Event.tsx
@@ -1,9 +1,4 @@
-/*
-    Event resizing code is modified from this stack overflow answer:
-    https://stackoverflow.com/questions/60792300/how-to-create-a-resizable-component-in-react
-*/
 
-import { useState, memo } from "react";
 import Draggable from 'react-draggable';
 import './Event.scss';
 
@@ -14,60 +9,15 @@ type eventProps = {
 
 
 const Event = (props: eventProps) => {
-    const [resize, setResize] = useState({
-        active: false,
-        y: 0
-    });
-    const [height, setHeight] = useState(96);
-    const eventStyle = {height: `${height}px`}
-
-
-    const handleMouseDown = (e: MouseEvent) => {
-        setResize({ 
-            active: true,
-            y: e.clientY
-        });
-    }
-
-
-    const handleMouseMove = (e: MouseEvent) => {
-        const { active, y } = resize;
-        if(active){
-            const yDiff: number = Math.abs(y - e.clientY);
-            const newHeight: number 
-                = y > e.clientY ? height - yDiff : height + yDiff;
-
-            setResize( {...resize, y: e.clientY});
-            setHeight(newHeight);
-        }
-    }
-
-
-    const handleMouseUp = () => {
-        setResize({...resize, active: false});
-    }
-
     return (
-        <Draggable axis='y' handle={".Event__reposition"}key={props.title}>
-            <div className="Event" style={eventStyle}>
+        <Draggable axis='y' handle={".Event__reposition"} key={props.title}>
+            <div className="Event">
                 <h3>{props.title}</h3>
                 <p>{props.time}</p>
-                <div 
-                    className="Event__reposition"></div>
-                <div 
-                    className="Event__resize "
-                    onMouseDown={handleMouseDown}
-                    onMouseMove={handleMouseMove}
-                    onMouseUp={handleMouseUp}></div>
-                
+                <div className="Event__reposition"></div>
             </div>
         </Draggable>
     )
 }
 
-const alwaysCallPropsEqual = () => {
-    return true;
-}
-
-const memoizedEvent = memo(Event, alwaysCallPropsEqual);
-export default memoizedEvent;
+export default Event;
