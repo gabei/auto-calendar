f37cc8c2f1b3bfe75e0ec893e3815a9539bb22d7
gabei <gabe1792@gmail.com>
2024-06-17 16:50:59 -0500

    adjusted heights and widths of calendar container

--- client/src/Event/Event.tsx
+++ client/src/Event/Event.tsx
@@ -7,10 +7,18 @@ type eventProps = {
     time: string,
 }
 
+function handleDrag() {
+    console.log(this.getPropertyValue("translate"));
+}
+
 
 const Event = (props: eventProps) => {
     return (
-        <Draggable axis='y' handle={".Event__reposition"} key={props.title}>
+        <Draggable 
+            axis='y' 
+            handle={".Event__reposition"} 
+            key={props.title}
+            grid=[0,20]>
             <div className="Event">
                 <h3>{props.title}</h3>
                 <p>{props.time}</p>
